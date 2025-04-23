285ae90a52325946bbbb086f393e99ea885da795
Abaljerind <abaljerinda@gmail.com>
2025-04-23 22:09:21 +0700

    Created component Header and received props from App
    Used icons from react-icons
    Created div for each pieces like logo, menu icon, mobile menu, desktop menu and request invite
    Gave styles for each elements

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,94 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { IoMenuOutline } from "react-icons/io5";
+
+const NavBar = ({ handleNav, nav }) => {
+  return (
+    <div className="relative flex items-center justify-between bg-white p-6 shadow-xs">
+      {/* Logo */}
+      <div>
+        <img src="/images/logo.svg" alt="Logo easybank" />
+      </div>
+      {/* end Logo */}
+
+      {/* menu icon */}
+      <div className="cursor-pointer text-2xl md:hidden">
+        {nav ? (
+          <AiOutlineClose onClick={handleNav} />
+        ) : (
+          <IoMenuOutline onClick={handleNav} />
+        )}
+      </div>
+      {/* end menu icon*/}
+
+      {/* Mobile menu */}
+      <div
+        className={`absolute top-24 left-0 w-full origin-top md:hidden ${nav ? "scale-y-100" : "scale-y-0"} flex justify-center duration-300`}
+      >
+        <ul className="flex w-[85%] flex-col items-center justify-between gap-3 rounded-md bg-white py-6 shadow-md">
+          <li className="w-full cursor-pointer text-center text-lg">
+            <a href="#" className="text-dark-blue font-medium">
+              Home
+            </a>
+          </li>
+          <li className="w-full cursor-pointer text-center text-lg">
+            <a href="#" className="text-dark-blue font-medium">
+              About
+            </a>
+          </li>
+          <li className="w-full cursor-pointer text-center text-lg">
+            <a href="#" className="text-dark-blue font-medium">
+              Contact
+            </a>
+          </li>
+          <li className="w-full cursor-pointer text-center text-lg">
+            <a href="#" className="text-dark-blue font-medium">
+              Blog
+            </a>
+          </li>
+          <li className="w-full cursor-pointer text-center text-lg">
+            <a href="#" className="text-dark-blue font-medium">
+              Careers
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* end Mobile menu */}
+
+      {/* desktop menu */}
+      <ul className="hidden justify-evenly gap-4 md:flex lg:gap-8">
+        <li>
+          <a href="#" className="text-dark-blue font-medium">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-dark-blue font-medium">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-dark-blue font-medium">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-dark-blue font-medium">
+            Blog
+          </a>
+        </li>
+        <li>
+          <a href="#" className="text-dark-blue font-medium">
+            Careers
+          </a>
+        </li>
+      </ul>
+      {/* end desktop menu */}
+
+      {/* request invite */}
+      <div className="hidden md:block">asdadanml</div>
+      {/* end request invite */}
+    </div>
+  );
+};
+
+export default NavBar;

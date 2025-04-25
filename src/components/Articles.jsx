4e5b32473a9fa38ddd59619140928aa30ffbc6de
Abaljerind <abaljerinda@gmail.com>
2025-04-25 22:12:03 +0700

    Created an Articles component
    Divided into 2 sections: header and article list
    Created a variable array containing 4 objects
    Header section includes a heading (h2)
    Article section contains maps over the array and renders:
    - an img, a heading (h4) and 2 paragraphs (p) for the author name and article description for each object

---
+++ src/components/Articles.jsx
@@ -0,0 +1,76 @@
+const Articles = () => {
+  const articles = [
+    {
+      image: "/images/image-currency.jpg",
+      author: "By Claire Robinson",
+      title: "Receive money in any currency with no fees",
+      description:
+        "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+    },
+    {
+      image: "/images/image-restaurant.jpg",
+      author: "By Wilson Hutton",
+      title: "Treat yourself without worrying about money",
+      description:
+        "Our simple budgeting article allows you to separate out your spending and set realistic limits each month. That means you …",
+    },
+    {
+      image: "/images/image-plane.jpg",
+      author: "By Wilson Hutton",
+      title: "Take your Easybank card wherever you go",
+      description:
+        "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
+    },
+    {
+      image: "/images/image-confetti.jpg",
+      author: "By Claire Robinson",
+      title: "Our invite-only Beta accounts are now live!",
+      description:
+        "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site …",
+    },
+  ];
+
+  return (
+    <div className="bg-very-light-gray px-4 py-16">
+      {/* header */}
+      <div className="mb-12 text-center">
+        <h2 className="text-dark-blue mb-4 text-3xl font-medium">
+          Latest Articles
+        </h2>
+      </div>
+      {/* ./ header */}
+
+      {/* article */}
+      <div className="grid">
+        {articles.map((article, index) => {
+          return (
+            <div
+              key={index + 1}
+              className="mb-8 flex flex-col gap-2 overflow-hidden rounded-md bg-white shadow-xs last:mb-0"
+            >
+              <img
+                src={article.image}
+                alt={article.title}
+                className="h-[220px] w-full object-cover"
+              />
+              <div className="p-5">
+                <p className="text-grayish-blue mb-2 text-xs">
+                  {article.author}
+                </p>
+                <h4 className="text-dark-blue mb-2 text-lg leading-6 font-medium">
+                  {article.title}
+                </h4>
+                <p className="text-grayish-blue text-sm">
+                  {article.description}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      {/* ./ article */}
+    </div>
+  );
+};
+
+export default Articles;

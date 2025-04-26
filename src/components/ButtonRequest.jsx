59ee9678990b9268e47745c2f767692c119470c9
Abaljerind <abaljerinda@gmail.com>
2025-04-26 12:17:20 +0700

    Created a ButtonRequest component that contains a button tag

---
+++ src/components/ButtonRequest.jsx
@@ -0,0 +1,12 @@
+const ButtonRequest = () => {
+  return (
+    <button
+      type="button"
+      className="from-lime-green to-bright-cyan cursor-pointer rounded-full bg-linear-to-r/hsl px-6 py-2.5 font-bold text-white"
+    >
+      Request Invite
+    </button>
+  );
+};
+
+export default ButtonRequest;

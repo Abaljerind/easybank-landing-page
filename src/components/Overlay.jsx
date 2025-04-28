e10d18fca6b5e80d70ac496cb573647d266192ea
Abaljerind <abaljerinda@gmail.com>
2025-04-28 20:54:35 +0700

    Add utility class z-10 to the div tag

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -3,7 +3,7 @@ const Overlay = ({ nav }) => {
     <div
       className={
         nav
-          ? `fixed inset-0 h-screen bg-linear-to-b from-black/30 from-100% to-white duration-300 ease-in md:hidden ${nav ? "scale-y-100" : "scale-y-0"}`
+          ? `fixed inset-0 z-10 h-screen bg-linear-to-b from-black/30 from-100% to-white duration-300 ease-in md:hidden ${nav ? "scale-y-100" : "scale-y-0"}`
           : ""
       }
     ></div>

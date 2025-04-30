a0d5cdfa2d354de0c2718c9f529dfe02c4ad3e3f
Abaljerind <abaljerinda@gmail.com>
2025-04-30 20:44:22 +0700

    Applied hover states

--- src/components/ButtonRequest.jsx
+++ src/components/ButtonRequest.jsx
@@ -2,7 +2,7 @@ const ButtonRequest = () => {
   return (
     <button
       type="button"
-      className="from-lime-green to-bright-cyan cursor-pointer rounded-full bg-linear-to-r/hsl px-8 py-2.5 font-bold text-white"
+      className="from-lime-green to-bright-cyan cursor-pointer rounded-full bg-linear-to-r/hsl px-8 py-2.5 font-bold text-white duration-300 ease-in hover:brightness-110"
     >
       Request Invite
     </button>

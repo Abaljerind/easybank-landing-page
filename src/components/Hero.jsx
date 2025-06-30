f62b95c168473b85607c8d5fc7efdf5e12c53011
Abaljerind <abaljerinda@gmail.com>
2025-06-30 14:25:40 +0700

    Style(Hero): adjust styles for lg breakpoint
    Adjust padding-left to <section>
    Adjust left value to <img> of background and mockups
    Adjust space-y value to <div> of container of hero text and <div> of text wrapper of hero text
    Adjust text wrap to <p>
    Add <br> and adjust display for mobile and lg breakpoint

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import ButtonRequest from "./ButtonRequest";
 
 const Hero = () => {
   return (
-    <section className="pb-24 md:flex md:flex-row-reverse md:overflow-hidden">
+    <section className="pb-24 md:flex md:flex-row-reverse md:overflow-hidden lg:pl-8">
       {/* hero img */}
       <div className="md:w-full md:pb-60">
         <figure className="relative h-[325px] min-[375px]:min-h-[385px] md:min-h-0">
@@ -15,7 +15,7 @@ const Hero = () => {
             <img
               src="./images/bg-intro-mobile.svg"
               alt="Decorative background for Hero section"
-              className="w-full min-[425px]:max-w-[426px] md:absolute md:top-0 md:left-10 md:scale-200"
+              className="w-full min-[425px]:max-w-[426px] md:absolute md:top-0 md:left-10 md:scale-200 lg:left-52"
             ></img>
           </picture>
           {/* ./ background */}
@@ -24,7 +24,7 @@ const Hero = () => {
           <img
             src="./images/image-mockups.png"
             alt="Phone screen mockups of Easybank app"
-            className="absolute -top-20 drop-shadow-xl min-[321px]:scale-95 min-[425px]:max-w-[426px] md:-top-12 md:-left-4 md:scale-100"
+            className="absolute -top-20 drop-shadow-xl min-[321px]:scale-95 min-[425px]:max-w-[426px] md:-top-12 md:-left-4 md:scale-100 lg:left-[120px]"
           />
           {/* ./ mockups */}
         </figure>
@@ -32,14 +32,16 @@ const Hero = () => {
       {/* ./ hero img */}
 
       {/* hero text */}
-      <div className="space-y-4 px-4 text-center max-[700px]:mt-0 min-[400px]:mt-12 md:text-start">
-        <div className="space-y-4">
+      <div className="space-y-4 px-4 text-center max-[700px]:mt-0 min-[400px]:mt-12 md:text-start lg:space-y-6">
+        <div className="space-y-4 lg:space-y-6">
           <h1 className="text-dark-blue text-4xl leading-11 font-medium">
             Next generation <br /> digital banking
           </h1>
-          <p className="text-grayish-blue md:text-balance">
-            Take your financial life online. Your Easybank account will be a
-            one-stop-shop for spending, saving, budgeting, investing, and much
+          <p className="text-grayish-blue md:text-balance lg:text-nowrap">
+            Take your financial life online. Your Easybank account{" "}
+            <br className="hidden lg:block" /> will be a one-stop-shop for
+            spending, saving,
+            <br className="hidden lg:block" /> budgeting, investing, and much
             more.
           </p>
         </div>
